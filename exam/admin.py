23d5d6e2eb908bb163ae4680e86fe486225b195f
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-06-21 22:43:53 +0300

    Registered models to admin panel

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+"""
+    Register all models at once
+"""
+from django.apps import apps
+
+
+models = apps.get_models()
+
+for model in models:
+    admin.site.register(model)
