e9aafdcbc4ec5ad237a05db5fdf098b1fd35c764
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-06-21 22:33:03 +0300

    Implemented ERD

--- question/models.py
+++ question/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from abstract_models import timestamp
+from exam.models import Exam
 
-# Create your models here.
+
+class Question(timestamp.TimeStamp):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question = models.CharField(max_length=1024)
--- student_answer/models.py
+++ student_answer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from abstract_models import timestamp
+from exam.models import Exam
+from question.models import Question
 
-# Create your models here.
+
+class UserAnswer(timestamp.TimeStamp):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+
+
+class Answer(timestamp.TimeStamp):
+    user_answer = models.ForeignKey(UserAnswer, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2048)
+
