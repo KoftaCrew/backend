406761819a93e10e4c5225ff137120d0d1a6d15f
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-06-26 00:10:28 +0300

    Fixed ERD & altered relations

--- exam/models.py
+++ exam/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from abstract_models import timestamp
+from django.utils.translation import gettext_lazy as _
 
 
 class Exam(timestamp.TimeStamp):
+    class ExamModes(models.IntegerChoices):
+        IDLE = 0, _("idle")
+        EDITING = 1, _("edit")
+        RESULTS = 2, _("result")
+
     name = models.CharField(max_length=255)
+    description = models.CharField(max_length=2048, null=True)
+    mode = models.IntegerField(
+        choices=ExamModes.choices,
+        default=ExamModes.IDLE
+    )
--- question/serializers.py
+++ question/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from question.models import Question
+from model_answer.serializers import ModelAnswerSerializer
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    question_model_answer = ModelAnswerSerializer(many=False)
+
     class Meta:
         model = Question
         fields = '__all__'
