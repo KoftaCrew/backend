aa978d6fbfcba1e274010e555bc83543efcdb6c8
M-AshrafDawood <ashraf.dawood2295@gmail.com>
2023-06-29 02:45:19 +0300

    Fixed permission issue for model_answer

--- model_answer/views.py
+++ model_answer/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
 from model_answer.serializers import ModelAnswerSerializer, KeyPhraseSerializer
 from model_answer.models import ModelAnswer, KeyPhrase
+from question.models import Question
+from exam.models import Exam
+from rest_framework import response
 
 
 # Create your views here.
@@ -12,10 +15,20 @@ class ModelAnswerViewSet(viewsets.ModelViewSet):
     lookup_field = 'id'
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        return self.queryset.filter(question__exam__user_id=self.request.user.id)
+
+    def create(self, request, *args, **kwargs):
+        exam_id =  Question.objects.get(id=request.data.get('question', -1)).exam_id
+        if  Exam.objects.get(id=exam_id).user_id == request.user.id:
+            return super().create(request, *args, **kwargs)
+        return response.Response({
+            'details': "You aren't able to edit this exam"
+        })
+
 
 class KeyPhraseViewSet(viewsets.ModelViewSet):
     queryset = KeyPhrase.objects.all().order_by('-created_at')
     serializer_class = KeyPhraseSerializer
     lookup_field = 'id'
     permission_classes = [permissions.IsAuthenticated]
-
